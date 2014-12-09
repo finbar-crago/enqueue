81d086e9bcbc6242dc959fb995d769b5d9cdaa26
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2014-12-10 10:59:51 +1100

    update agent kpi format

--- www/lib/EnQ/Metrics.pm
+++ www/lib/EnQ/Metrics.pm
@@ -3,7 +3,8 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub main {
   my $self = shift;
-  my $agents = int(rand()*25);
+  my $aTotal = 26;
+  my $agents = sprintf("%d/%d", $aTotal-int(rand()*26),$aTotal);
   my $calls = int(rand()*25);
   my $drops = int(rand()*25);
   my $wTime = sprintf("%02d:%02d", int(rand()*59),int(rand()*59));
