5226118e32aecb48c99eb84a92be4e548ede28cb
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2014-12-22 16:25:30 +1100

    update ast/setup.sh
    + only download if no dir
    + switch curl to wget for compatibility

--- ast/setup.sh
+++ ast/setup.sh
@@ -3,8 +3,10 @@
 AST_URL="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz"
 AST_DIR="/usr/src/asterisk-13"
 
-#mkdir -p $AST_DIR
-#curl -L $AST_URL | tar xzf - -C $AST_DIR --strip-components=1
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir -p $AST_DIR
+    wget -O - $AST_URL | tar xzf - -C $AST_DIR --strip-components=1
+fi
 
 cd $AST_DIR
 ./configure
