eaf0c9f58f65abb23575596a0cf62e40b3cc41e4
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2014-12-26 22:36:30 +1100

    ast setup.sh stuff + update .gitignore for build dir

--- ast/setup.sh
+++ ast/setup.sh
@@ -2,21 +2,21 @@
 
 function mk_pjsip {
     PJSIP_URL="http://www.pjsip.org/release/2.3/pjproject-2.3.tar.bz2"
-    PJSIP_DIR="/usr/src/pjsip-2.3"
+    PJSIP_DIR="./build/pjsip-2.3"
 
     if [ ! -d "$PJSIP_DIR" ]; then
 	mkdir -p $PJSIP_DIR
-	wget -O - $PJSIP_DIR | tar xzf - -C $PJSIP_DIR --strip-components=1
+	wget -O - $PJSIP_URL | tar jxf - -C $PJSIP_DIR --strip-components=1
     fi
 
     cd $PJSIP_DIR
     ./configure
+    make dep && make && make install
 }
-
 
 function mk_asterisk {
     AST_URL="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz"
-    AST_DIR="/usr/src/asterisk-13"
+    AST_DIR="./build/asterisk-13"
 
     if [ ! -d "$AST_DIR" ]; then
 	mkdir -p $AST_DIR
@@ -25,6 +25,7 @@ function mk_asterisk {
 
     cd $AST_DIR
     ./configure
+    make && make install
 }
 
 mk_pjsip
