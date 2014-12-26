57039b911a0c2696d540e756a8b3dec27609ee41
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2014-12-26 21:43:22 +1100

    cleanup ast + dep build script...

--- ast/setup.sh
+++ ast/setup.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-AST_URL="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz"
-AST_DIR="/usr/src/asterisk-13"
+function mk_pjsip {
+    PJSIP_URL="http://www.pjsip.org/release/2.3/pjproject-2.3.tar.bz2"
+    PJSIP_DIR="/usr/src/pjsip-2.3"
 
-if [ ! -d "$DIRECTORY" ]; then
-    mkdir -p $AST_DIR
-    wget -O - $AST_URL | tar xzf - -C $AST_DIR --strip-components=1
-fi
+    if [ ! -d "$PJSIP_DIR" ]; then
+	mkdir -p $PJSIP_DIR
+	wget -O - $PJSIP_DIR | tar xzf - -C $PJSIP_DIR --strip-components=1
+    fi
 
-cd $AST_DIR
-./configure
+    cd $PJSIP_DIR
+    ./configure
+}
+
+
+function mk_asterisk {
+    AST_URL="http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz"
+    AST_DIR="/usr/src/asterisk-13"
+
+    if [ ! -d "$AST_DIR" ]; then
+	mkdir -p $AST_DIR
+	wget -O - $AST_URL | tar xzf - -C $AST_DIR --strip-components=1
+    fi
+
+    cd $AST_DIR
+    ./configure
+}
+
+mk_pjsip
