bd4c8a7d988c34255ba3bff68604b4ff3e7ce258
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2014-12-27 18:14:36 +1100

    start asterisk config generation libs

---
+++ lib/AstConf.pm
@@ -0,0 +1,97 @@
+pakage AstConf;
+use strict;
+use warnings;
+
+use constant {
+    READ    => 1,
+    WRITE   => 2,
+    REQURED => 4,
+    REGEX   => 8,
+    CB      => 16
+};
+
+sub _init {
+    my $mod  = shift;
+    my $self =
+    {
+	id     => undef,
+	parent => undef,
+	type   => undef,
+	data   => $mod->{'data'} || undef,
+	db     => $mod->{'db'}   || undef,
+	_error => undef,
+    };
+
+
+    my $_closure = sub{
+	my $field = shift;
+	return $self->{'data'}{$field} if $field =~ /^_/;
+	$self->{'_error'} = undef;
+
+	if(@_){ #write var
+
+	    if($self->{'data'}{$field} && $self->{'data'}{$field}{'mode'} & WRITE){
+		my $value = shift;
+
+		if($self->{'data'}{$field}{'mode'} & REGEX){
+		    my $regex = $self->{'data'}{$field}{'regex'};
+		    if($value =~ m/$regex/){
+			$self->{'data'}{$field}{'value'} = $value;
+			return $value;
+		    } else {
+			$self->{'_error'} = 'faild regex';
+			return undef;
+		    }
+		}
+
+		if($self->{'data'}{$field}{'mode'} & CB){
+		    my $r = &{ $self->{'data'}{$field}{'cb'} }($value);
+		    if($r){
+			$self->{'data'}{$field}{'value'} = $r;
+			return $r;
+		    } else {
+			$self->{'_error'} = 'faild callback';
+		    }
+		}
+
+		$self->{'data'}{$field}{'value'} = $value;
+		return $value;
+
+	    } else {
+		$self->{'_error'} = 'cannot write';
+		return undef;
+	    }
+	} else { #read var
+	    if($self->{'data'}{$field} && $self->{'data'}{$field}{'mode'} & READ){
+		return $self->{'data'}{$field}{'value'};
+	    } else {
+		$self->{'_error'} = 'cannot read';
+		return undef;
+	    }
+	}
+    };
+
+    return $_closure;
+}
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    my $closure = _init($self);
+    return bless $closure, $class;
+}
+
+
+sub NewAcc {
+    my $self = shift;
+    ($uid, $exten, $auth) = @_;
+
+}
+
+
+sub AUTOLOAD {
+    our $AUTOLOAD =~ s/.*:://;
+    &{ $_[0] }($AUTOLOAD, @_[1 .. $#_ ] )
+}
+
+1;
