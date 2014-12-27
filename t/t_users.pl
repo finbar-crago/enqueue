0a510abcc11b8011b00a21ba581272cff30de96c
Finbar Crago <finbar.crago@gmail.com>
2014-12-27 22:07:48 +1100

    start test for EnQ::Users

---
+++ t/t_users.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use FindBin;
+BEGIN { unshift @INC, "$FindBin::Bin/../lib" }
+
+use EnQ::Users
+$\ = "\n"; $, = "\t";
+
+my $u = EnQ::Users->new(); 
+$u->uid("finbar") or print $u->is_error();
+$u->extn("12345") or print $u->is_error();
+
+print "user:", $u->uid;
+print "hash:", $u->extn;
+
+$u->extn("ABCD") or print $u->is_error();
