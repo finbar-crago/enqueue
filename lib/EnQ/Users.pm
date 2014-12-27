67c0fabe6c63865a70b40349ececef5d20ca266e
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2014-12-27 18:40:33 +1100

    start users lib

---
+++ lib/EnQ/Users.pm
@@ -0,0 +1,22 @@
+package EnQ::Users;
+use strict;
+use warnings;
+
+use parent qw(EnQ);
+use constant { READ => 1, WRITE => 2, REQURED => 4, REGEX => 8, CB => 16 };
+
+my $Object = {
+    data => {
+	uid  => {mode => READ|WRITE},
+	extn => {mode => READ|WRITE},
+    },
+    db => { table  => 'users', },
+};
+
+sub new {
+    my $class = shift;
+    my $closure = EnQ::_init($Object);
+    return bless $closure, $class;
+}
+
+1;
