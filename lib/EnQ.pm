4f87c9caf5f837af61353b69e69dfd800e8b274b
Finbar Crago <finbar.crago@gmail.com>
2014-12-30 20:34:17 +1100

    start of EnQ.pm

--- lib/EnQ.pm
+++ lib/EnQ.pm
@@ -2,4 +2,16 @@ package EnQ;
 use strict; 
 use warnings;
 
+use parent qw(EnQ::Object);
+
+sub new {
+    my $class = shift;
+
+    my $Object = {
+	data => {},
+    };
+    my $closure = EnQ::Object::_init($Object);
+    return bless $closure, $class;
+}
+
 1;
