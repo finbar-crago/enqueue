10d0ea031fb255d02c4c63fadf6f755c37848613
Finbar Crago <finbar.crago@gmail.com>
2014-12-31 21:03:00 +1100

    add user routers to WWW.pm

--- lib/EnQ/WWW.pm
+++ lib/EnQ/WWW.pm
@@ -9,6 +9,11 @@ sub startup {
   $r->get('/')->to('Dashboard#main');
 
   $r->get('/api/metrics')->to('Metrics#main');
+
+  $r->get('/api/user')->to('Users#list');
+  $r->get('/api/user/:uid')->to('Users#info');
+  $r->post('/api/user')->to('Users#new');
+
 }
 
 1;
