19df82428a89a89a6d9af38d9d0a624265767419
Finbar Crago <finbar.crago@gmail.com>
2015-01-05 23:40:15 +1100

    add perldoc to EnQ.pm

--- lib/EnQ.pm
+++ lib/EnQ.pm
@@ -2,6 +2,43 @@ package EnQ;
 use strict; 
 use warnings;
 
+=head1 NAME
+
+EnQ - The Enqueue PBX Libraries
+
+=head1 SYNOPSIS
+
+  use EnQ;
+
+  my $Q = EnQ->new({config => 'my_config.yml');
+
+  my $U = $Q->Obj('User');
+  $U->pull('user_id');
+  print $U->name;
+
+=head1 AUTHOR
+
+Finbar Crago <finbar.crago@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2015 Finbar Crago
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+=cut
+
 use EnQ::DBA;
 use YAML;
 
