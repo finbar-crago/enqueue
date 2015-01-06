8ad324cd82d0a38148b645930812b6861829ae92
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2015-01-06 14:43:53 +1100

    add perldocs for EnQ::DBA

--- lib/EnQ/DBA.pm
+++ lib/EnQ/DBA.pm
@@ -2,6 +2,46 @@ package EnQ::DBA;
 use strict;
 use warnings;
 
+=head1 NAME
+
+EnQ::DBA - Enqueue PBX Database Stuff
+
+=head1 SYNOPSIS
+
+  package EnQ::Obj::Example;
+  use parent qw(EnQ::Object);
+
+  my $Object = {
+    db => {
+      key   => 'uid',
+      table => 'table_name',
+      setup => 'CREATE TABLE table_name (uid TEXT PRIMARY KEY, name TEXT)',
+    },
+  };
+
+=head1 AUTHOR
+
+Finbar Crago <finbar.crago@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014-2015 Finbar Crago
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+=cut
+
 use DBI;
 
 my $SQL = {
