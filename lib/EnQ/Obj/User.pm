9c196ee5ed08826a8a9d38199c94bfa4a1d12dfe
Finbar Crago <finbar.crago@gmail.com>
2015-01-10 12:03:53 +1100

    Add docs to EnQ::Obj::User

--- lib/EnQ/Obj/User.pm
+++ lib/EnQ/Obj/User.pm
@@ -2,6 +2,45 @@ package EnQ::Obj::User;
 use strict;
 use warnings;
 
+=head1 NAME
+
+EnQ::DBA - Enqueue PBX Database Stuff
+
+=head1 SYNOPSIS
+
+  package EnQ;
+  my $Q = EnQ->new({config_file => 'config.yml'});
+
+  my $U = $Q->Obj('User');
+  $U->Data({
+  	uid=>'finbar',
+  	name=>'Finbar Cragp',
+  	extn=>'5377',
+  	pass=>'myPass',
+  });
+
+  print $U->name if $U->chackPass('tryPass');
+  
+=head1 AUTHOR
+
+Finbar Crago <finbar.crago@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2014, 2015 Finbar Crago
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see L<http://www.gnu.org/licenses/>.
+
+=cut
+
 use Digest;
 
 use parent qw(EnQ::Object);
