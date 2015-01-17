d33d00699e6bbefbff29f047178843aa4b7f72e5
Finbar Crago <finbar.crago@gmail.com>
2015-01-18 02:40:44 +1100

    start angularjs routing for all html pages!!!

---
+++ www/public/main.js
@@ -0,0 +1,29 @@
+var EnQ = angular.module('EnQ', ['ngRoute']);
+
+EnQ.config(['$routeProvider',
+function($routeProvider) {
+    $routeProvider.
+
+	when('/admin/agents', {
+	    templateUrl: 'test.html',
+	    controller:  'UsersCtrl'
+	}).
+
+	otherwise({
+	    redirectTo: '/home'
+	});
+}]);
+
+
+EnQ.controller('UsersCtrl', function ($scope, $http){
+    $scope.users = [];
+    $http.get('/api/users').success(function(data){
+        data.list.forEach(function(uid){
+            console.log('get /api/user/'+uid);
+            $http.get('/api/user/'+uid).success(function(u){
+                $scope.users.push( u.data );
+            });
+        });
+        $scope.orderProp = 'uid';
+    });
+});
