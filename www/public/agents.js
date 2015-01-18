de2b64952f3fc48080b63ac0191e0c6ffdf03c2a
Finbar Crago <finbar.crago@gmail.com>
2015-01-19 04:00:34 +1100

    agent edit button now does things!

--- www/public/agents.js
+++ www/public/agents.js
@@ -1,15 +1,26 @@
+var EnqCtrl = angular.module('EnqCtrl', []);
 
-EnQ.controller('UsersCtrl', function ($rootScope, $scope, $http){
+EnqCtrl.controller('UsersCtrl', ['$rootScope', '$scope', '$routeParams', '$http',
+function ($rootScope, $scope, $routeParams, $http){
+
     $rootScope.title = 'Agents';
-
+    var uid = $routeParams.uid;
+    $scope.data  = [];
     $scope.users = [];
+    $scope.user  = {};
+
     $http.get('/api/users').success(function(data){
-        data.list.forEach(function(uid){
-            console.log('get /api/user/'+uid);
-            $http.get('/api/user/'+uid).success(function(u){
-                $scope.users.push( u.data );
+        data.list.forEach(function(id){
+            $http.get('/api/user/'+id).success(function(u){
+                $scope.data[u.data.uid] = u.data;
+		$scope.users.push(u.data.uid)
+		if(u.data.uid == uid){
+		    $rootScope.title = 'Agent (' + uid + ')';
+		    $scope.user = u.data;
+		    $scope.user.pass =  '******';
+		}
             });
         });
-        $scope.orderProp = 'uid';
+
     });
-});
+}]);
--- www/public/main.js
+++ www/public/main.js
@@ -1,10 +1,15 @@
-var EnQ = angular.module('EnQ', ['ngRoute']);
+var EnQ = angular.module('EnQ', ['ngRoute', 'EnqCtrl']);
 
 EnQ.config(['$routeProvider',
 function($routeProvider) {
     $routeProvider.
 
 	when('/admin/agents', {
+	    templateUrl: 'agents.html',
+	    controller:  'UsersCtrl'
+	}).
+
+	when('/admin/agent/:uid', {
 	    templateUrl: 'agents.html',
 	    controller:  'UsersCtrl'
 	}).
