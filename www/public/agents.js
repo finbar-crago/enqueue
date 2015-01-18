80e28570f82cea35a9b2e03f62f8eb8ba005adcc
Finbar Crago <finbar.crago@gmail.com>
2015-01-19 05:57:39 +1100

    add error div

--- www/public/agents.js
+++ www/public/agents.js
@@ -5,14 +5,27 @@ function ($rootScope, $scope, $routeParams, $http){
 
     $rootScope.title = 'Agents';
     var uid = $routeParams.uid;
-    $scope.data   = [];
-    $scope.users  = [];
-    $scope.user   = {};
-    $scope.isEdit = false;
+    $scope.data    = [];
+    $scope.users   = [];
+    $scope.user    = {};
+    $scope.isEdit  = false;
+    $scope.allGood = true;
+    $scope.error   = '';
 
     $http.get('/api/users').success(function(data){
         data.list.forEach(function(id){
+	    if(data.status == 'ERROR'){
+		$scope.allGood = false;
+		$scope.error = r.error;
+		return;
+	    }
             $http.get('/api/user/'+id).success(function(u){
+		if(u.status == 'ERROR'){
+		    $scope.allGood = false;
+		    $scope.error = r.error;
+		    return;
+		}
+
                 $scope.data[u.data.uid] = u.data;
 		$scope.users.push(u.data.uid)
 		if(u.data.uid == uid){
@@ -29,7 +42,10 @@ function ($rootScope, $scope, $routeParams, $http){
         if ($scope.user) {
 	    console.log($scope.user);
 	    $http.post('/api/user/', $scope.user).success(function(r){
-		console.log(r);
+		if(r.status == 'ERROR'){
+		    $scope.allGood = false;
+		    $scope.error = r.error;
+		}
 	    });
         }
     };
