a84f496540add147a996bded8b20c299387f7a75
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2015-01-20 13:44:51 +1100

    start of queues.html

--- www/public/queues.js
+++ www/public/queues.js
@@ -4,5 +4,6 @@ EnqCtrl.controller('QueuesCtrl', ['$rootScope', '$scope', '$routeParams', '$http
 function ($rootScope, $scope, $routeParams, $http){
 
     $rootScope.title = 'Queues';
+    $scope.allGood   =     true;
 
 }]);
