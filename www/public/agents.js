0b5337791a3a2f570cd3314d06d7bae18aa9fc29
Finbar Crago <finbar.crago@gmail.com>
2015-01-24 23:03:57 +1100

    Rewrite angularjs code!

--- www/public/agents.js
+++ www/public/agents.js
@@ -1,69 +1,57 @@
-var EnqCtrl = angular.module('EnqCtrlUsers', []);
 
-EnqCtrl.controller('UsersCtrl', ['$rootScope', '$scope', '$routeParams', '$http',
+function NewCtrl(name, rpc){
+var EnqCtrl = angular.module('EnqCtrl'+name, []);
+
+EnqCtrl.controller(name+'Ctrl', ['$rootScope', '$scope', '$routeParams', '$http',
 function ($rootScope, $scope, $routeParams, $http){
 
-    $rootScope.title = 'Agents';
     var uid = $routeParams.uid;
-    $scope.data    = [];
-    $scope.users   = [];
-    $scope.user    = {};
-    $scope.isEdit  = false;
-    $scope.allGood = true;
-    $scope.error   = '';
+    $rootScope.title = name;
+    $scope.uid   = uid;
+    $scope.data  = {};
+    $scope.error = null;
 
-    $http.get('/api/users').success(function(data){
-        data.list.forEach(function(id){
-	    if(data.status == 'ERROR'){
-		$scope.allGood = false;
-		$scope.error = r.error;
-		return;
-	    }
-            $http.get('/api/users/'+id).success(function(u){
-		if(u.status == 'ERROR'){
-		    $scope.allGood = false;
+    $http.get(rpc).success(function(obj){
+	if(obj.status != 'ok'){
+	    $scope.error = obj.error;
+	    return;
+	}
+
+        obj.list.forEach(function(i){
+            $http.get(rpc+i).success(function(j){
+		if(j.status != 'ok'){
 		    $scope.error = r.error;
 		    return;
 		}
-
-                $scope.data[u.data.uid] = u.data;
-		$scope.users.push(u.data.uid)
-		if(u.data.uid == uid){
-		    $rootScope.title = 'Agent (' + uid + ')';
-		    $scope.user = u.data;
-		    $scope.isEdit = true;
-		}
+                $scope.data[j.data.uid] = j.data;
             });
         });
     });
 
-    $scope.submit = function() {
-        if ($scope.user) {
-	    console.log($scope.user);
-	    $http.post('/api/users/'+uid, $scope.user).success(function(r){
-		if(r.status == 'ERROR'){
-		    $scope.allGood = false;
-		    $scope.error = r.error;
-		} else {
-		    $scope.isEdit = true;
-                    $scope.data[$scope.user.uid] = $scope.user;
-		    if($scope.users.indexOf($scope.user.uid) == -1){
-			$scope.users.push($scope.user.uid);
-		    }
-		}
-	    });
-        }
+    $scope.push = function() {
+	if(!$scope.data[uid]) return;
+	$http.post(rpc + uid, $scope.data[uid]).success(function(r){
+	    if(r.status != 'ok'){
+		$scope.error = r.error;
+		return;
+	    }
+	    $scope.uid = null;
+	});
     };
 
     $scope.purge = function(id){
-	$http.delete('/api/users/'+id).success(function(r){
-	    if(r.status == 'ERROR'){
-		$scope.allGood = false;
+	if(!id) return;
+	$http.delete(rpc + id).success(function(r){
+	    if(r.status != 'ok'){
 		$scope.error = r.error;
-	    } else {
-		$scope.users.splice($scope.users.indexOf(id),1);
+		return;
 	    }
+	    delete $scope.data[id];
 	});
     };
 
 }]);
+
+}
+
+NewCtrl('Users', '/api/users/');
