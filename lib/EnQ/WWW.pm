7e708f395c74a7fe823dec5a587797f188e56c13
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2015-01-27 12:56:07 +1100

    name routes

--- lib/EnQ/WWW.pm
+++ lib/EnQ/WWW.pm
@@ -11,14 +11,14 @@ sub startup {
   my $EnQ = EnQ::->new({config_file => $FindBin::Bin =~ s|(.+/enqueue).+|$1/misc/sample_config.yml|r});
   $self->helper(EnQ => sub { return $EnQ });
 
-  $r->get('/' => sub { shift->reply->static('index.html'); });
+  $r->get('/' => sub { shift->reply->static('index.html'); })->name('Static');
 
-  $r   ->get('/EnQ/@/:obj')     ->to('Search#List' );
-  $r  ->post('/EnQ/@/:obj')     ->to('Search#Query');
+  $r   ->get('/EnQ/@/:obj')     ->to('Search#List' )->name('SearchList' );
+  $r  ->post('/EnQ/@/:obj')     ->to('Search#Query')->name('SearchQuery');
 
-  $r   ->get('/EnQ/!/:obj/:uid')->to('Basic#Pull' );
-  $r  ->post('/EnQ/!/:obj/:uid')->to('Basic#Push' );
-  $r->delete('/EnQ/!/:obj/:uid')->to('Basic#Purge');
+  $r   ->get('/EnQ/!/:obj/:uid')->to('Basic#Pull' )->name('BasicPull' );
+  $r  ->post('/EnQ/!/:obj/:uid')->to('Basic#Push' )->name('BasicPush' );
+  $r->delete('/EnQ/!/:obj/:uid')->to('Basic#Purge')->name('BasicPurge');
 
 }
 
