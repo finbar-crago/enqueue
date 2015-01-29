fe603ac88da441f758d8d938de8d10792134ebf5
Finbar Crago <finbar.crago@gmail.com>
2015-01-29 12:00:34 +1100

    add DID Obj

---
+++ lib/EnQ/Obj/DID.pm
@@ -0,0 +1,21 @@
+package EnQ::Obj::DID;
+use strict;
+use warnings;
+
+use parent qw(EnQ::Object);
+
+our $Object = {
+    def => {
+	'did'    => EnQ::Object::Field(),
+	'act' => EnQ::Object::Field(),
+	'act_queue' => EnQ::Object::Field(),
+	'act_exten' => EnQ::Object::Field(),
+    },
+    db => {
+	key   => 'did',
+	table => 'did',
+	setup => 'CREATE TABLE did (did TEXT PRIMARY KEY, act TEXT, act_queue TEXT, act_exten TEXT)',
+    },
+};
+
+1;
