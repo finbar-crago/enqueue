630a22c48511a4ac1d00b3c2201d2ba2bea02f49
Finbar Crago <finbar@smartbusinesstelecom.com.au>
2015-04-09 12:47:18 +1000

    code cleanup

--- lib/EnQ/Ast/AutoIVR.pm
+++ lib/EnQ/Ast/AutoIVR.pm
@@ -9,13 +9,11 @@ sub conf_queues {
 
     print "[$ID](!)";
     print for(@{$y->{'QueueConf'}});
-    print '';
 
     for(@{$y->{'Queue'}}){
         print '['.$ID.'-'.$_->{'ID'}.'](base,'.$ID.')';
         print for(@{$_->{'Conf'}});
     }
-    print '';
 }
 
 sub conf_extensions_landing {
@@ -27,8 +25,6 @@ sub conf_extensions_landing {
         print 'exten => '.$_.',2,GoSub(IVR-'.$ID.')';
         print 'exten => '.$_.',3,Hangup';
     }
-
-    print '';
 }
 
 sub conf_extensions_agent {
@@ -69,8 +65,6 @@ sub conf_extensions_default {
         print 'exten => '.$_->{'Extn'}.',2,Dial(Local/',$_->{'To'},')';
         print 'exten => '.$_->{'Extn'}.',3,Hangup';
     }
-
-    print '';
 }
 
 sub conf_extensions_global {
@@ -83,14 +77,12 @@ sub conf_extensions_global {
     print 'exten => s,'.$pc++.",GoSub(IVR-TIME-$ID)";
     print 'exten => s,'.$pc++.",Playback($hello)";
     print 'exten => s,'.$pc++.",GoTo(IVR-OPT-$ID,s,1)";
-    print '';
 
     $pc = 1;
     print "[IVR-TIME-$ID]";
     print 'exten => s,'.$pc++.',ExecIfTime(',$y->{'Hours'},',mon-fri?Return)';
     print 'exten => s,'.$pc++.',ExecIfTime(',$y->{'HoursWE'},',sat-sun?Return)';
     print 'exten => s,'.$pc++.",GoTo(EXIT-CLOSED-$ID,s,1)";
-    print '';
 
     $pc = 1;
     my $prompts = $y->{'IVR'}->{'Play'}->{'Prompts'};
@@ -103,13 +95,13 @@ sub conf_extensions_global {
 	my $i = defined($OPT[$_])?"SUB-$ID-".$OPT[$_]:"IVR-OPT-$ID";
 	print 'exten => s,'.$pc++.",Set(HASH(OPT,$_)=$i)";
     }
+
     print 'exten => s,'.$pc++.",Set(HASH(OPT,#)=IVR-OPT-$ID)";
     print 'exten => s,'.$pc++.",Set(HASH(OPT,*)=IVR-OPT-$ID)";
     print 'exten => s,'.$pc++.",Set(HASH(OPT,)=SUB-$ID-".$y->{'DefaultQueue'}.')';
 
     print 'exten => s,'.$pc++.',GoSub(${HASH(OPT,${I})},PRIVACY)';
     print 'exten => s,'.$pc++.",GoTo(EXIT-FAILED-$ID,s,1)";
-    print '';
 
     for(@{$y->{'Queue'}}){
 	my $pc = 1;
@@ -119,25 +111,21 @@ sub conf_extensions_global {
 	print 'exten => s,'.$pc++.',ExecIf($["${ARG1}" = "PRIVACY"]?Playback('.$Privacy.'))';
 	print "exten => s,".$pc++.",Queue($ID-".$_->{'ID'}.")";
 	print "exten => s,".$pc++.",Return";
-	print '';
     }
 
     for(@{$y->{'Xfer'}}){
 	print "[SUB-$ID-".$_->{'ID'}."]";
 	print "exten => s,1,Dial(LOCAL/".$_->{'To'}.")";
 	print "exten => s,2,Return";
-	print '';
     }
 
     $pc = 1;
     print "[EXIT-CLOSED-$ID]";
     print 'exten => s,'.$pc++.",Hangup";
-    print '';
 
     $pc = 1;
     print "[EXIT-FAILED-$ID]";
     print 'exten => s,'.$pc++.",Hangup";
-    print '';
 }
 
 1;
